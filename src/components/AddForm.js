d8890621e547c9e14e63a2fc1fee97b616919dd1
pokrywa1 <h.pokrywka@wp.pl>
2022-11-08 21:04:40 +0100

    form validation

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from './Store/Modal';
-
+import classes from './AddForm.module.css';
 function AddForm(props) {
+  const [inputText, setInputText] = useState('');
+  const [isPositive, setIsPositive] = useState(false);
+
+  //check input value is Correct. Value must be longer than 0
+  useEffect(() => {
+    setIsPositive(() => {
+      return inputText.length > 0;
+    });
+  }, [inputText]);
+
+  const taskHandler = e => {
+    setInputText(e.target.value);
+  };
+  const buttonHandler = e => {
+    e.preventDefault();
+  };
   return (
     <Modal onClose={props.onClose}>
-      <p>hej</p>
-      <h1>hel;lo</h1>
+      <h2 className={classes.title}>Add your plans and tasks</h2>
+      <form onSubmit={buttonHandler} className={classes.task__form}>
+        <input
+          onChange={taskHandler}
+          type="text"
+          name="task"
+          id="task"
+          className={classes.placeholder}
+          placeholder="add task"
+        />
+
+        <input
+          disabled={!isPositive}
+          type="button"
+          className={classes.submit__button}
+          value="accept"
+          onClick={buttonHandler}
+        />
+      </form>
     </Modal>
   );
 }
