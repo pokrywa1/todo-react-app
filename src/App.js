22a317ca9e6e1b85c297265b4b3ac97476ac9b10
pokrywa1 <h.pokrywka@wp.pl>
2022-11-09 19:19:55 +0100

    add local storage and end project

--- src/App.js
+++ src/App.js
@@ -5,13 +5,21 @@ import AddForm from './components/AddForm';
 
 function App() {
   const [formIsShown, setFormIsShown] = useState(false);
-  const [todos, setTodos] = useState([]);
+  //getting data from local storage or set default empty array []
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem('todos');
+    const data = JSON.parse(saved);
+    console.log(data);
+    return data || [];
+  });
   const [doneTodos, setDoneTodos] = useState([]);
   useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
     const onlyDone = todos.filter(item => item.done);
     setDoneTodos(onlyDone);
     console.log(doneTodos);
   }, [todos]);
+
   const closeModalHandler = () => {
     setFormIsShown(false);
   };
